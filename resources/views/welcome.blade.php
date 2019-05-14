fa9b3e753195e3e834f36c53706ee6962e2ccc72
Lutermikel <luterapuestas@hotmail.com>
2019-05-14 23:30:17 +0000

    commit de prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Biblioteca de Laravel
                 </div>
 
                 <div class="links">
